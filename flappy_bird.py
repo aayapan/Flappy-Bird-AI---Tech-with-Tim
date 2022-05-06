b4d5a9dd0ffcaf384a5da1430d6c59a57b6debb0
Kkoli1 <kkolikkolikkoli@gmail.com>
2022-05-06 16:33:50 +0800

    test commit

--- flappy_bird.py
+++ flappy_bird.py
@@ -7,11 +7,13 @@ import random
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
 
+#   IMAGES
 BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird1.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird2.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird3.png")))]
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","pipe.png")))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bg.png")))
 
+#   BIRD CLASS
 class Bird:
     IMGS = BIRD_IMGS
     MAX_ROTATION = 25
